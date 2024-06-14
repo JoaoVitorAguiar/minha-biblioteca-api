5105e135997885abca9106b73d5b3c0c26535682
João Vitor Aguiar <121316279+JoaoVitorAguiar@users.noreply.github.com>
2024-06-14 19:01:27 -0300

    feat: add db contexts and setup to migrations

--- src/MinhaBiblioteca.Core/Entities/BaseEntity.cs
+++ src/MinhaBiblioteca.Core/Entities/BaseEntity.cs
@@ -1,15 +1,9 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace MinhaBiblioteca.Core.Entities;
+﻿namespace MinhaBiblioteca.Core.Entities;
 
 public abstract class BaseEntity
 {
     protected BaseEntity() { }
     public Guid Id { get; private set; }
     public DateTime CreatedAt { get; set; }
-    public DateTime UpdatedAt { get; set;}
+    public DateTime UpdatedAt { get; set; }
 }
---
+++ src/MinhaBiblioteca.Infrastructure/MinhaBibliotecaDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using MinhaBiblioteca.Core.Entities;
+
+namespace MinhaBiblioteca.Infrastructure;
+
+public class MinhaBibliotecaDbContext : DbContext
+{
+    public DbSet<User> Users { get; private set; }
+    public DbSet<Book> Books { get; private set; }
+    public DbSet<Loan> Loans { get; private set; }
+    public MinhaBibliotecaDbContext(DbContextOptions<MinhaBibliotecaDbContext> options) : base(options)
+    {
+    }
+}
